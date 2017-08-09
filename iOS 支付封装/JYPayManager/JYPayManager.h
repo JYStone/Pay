f4e9d8a6cbd645a2ce0c1e214c194426999276b1
jinjingyuan <jinjigyuan@huoqiu.cn>
2017-08-09 17:28:45 +0800

    commit

---
+++ iOS 支付封装/JYPayManager/JYPayManager.h
@@ -0,0 +1,13 @@
+//
+//  JYPayManager.h
+//  iOS 支付封装
+//
+//  Created by 金靖媛 on 2017/8/8.
+//  Copyright © 2017年 LY. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JYPayManager : NSObject
+
+@end
---
+++ iOS 支付封装/JYPayManager/JYPayManager.m
@@ -0,0 +1,13 @@
+//
+//  JYPayManager.m
+//  iOS 支付封装
+//
+//  Created by 金靖媛 on 2017/8/8.
+//  Copyright © 2017年 LY. All rights reserved.
+//
+
+#import "JYPayManager.h"
+
+@implementation JYPayManager
+
+@end
